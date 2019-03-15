70b47a10d2ca99d33b955accdcfb8985666eabc6
Aldobii <42969068+Aldobii@users.noreply.github.com>
2019-03-15 12:59:12 +0100

    fsvfjudf

---
+++ Someren1819 (Ram)/SomerenUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomerenUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SomerenUI());
+        }
+        //public static Control ShowTeachers()
+        //{
+        //    ListView teachersList = new ListView();
+        //    teachersList.Height =350;
+        //    teachersList.Width = 600;
+        //    return teachersList;
+        //}
+    }
+}
